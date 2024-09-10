f4eec8c9fa3443de5624b7cf35a81adcb4bc1050
GitHub Action <action@github.com>
2024-09-10 19:41:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/snuxoll/intellijazuredbauth/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/snuxoll/intellijazuredbauth/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.snuxoll.intellijazuredbauth.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.snuxoll.intellijazuredbauth.MyBundle
+import com.github.snuxoll.intellijazuredbauth.services.MyProjectService
 import javax.swing.JButton
 
 
